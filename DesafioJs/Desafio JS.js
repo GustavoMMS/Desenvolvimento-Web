4e84e95383328e440db8391211bfad2438a125f2
GustavoMMS <93543187+GustavoMMS@users.noreply.github.com>
2021-11-10 13:50:07 -0300

    Add files via upload

---
+++ DesafioJs/Desafio JS.js
@@ -0,0 +1,9 @@
+function altera1(){
+    var imagem=document.getElementById("altera")
+    imagem.setAttribute('src', 'acesa.jpg')
+}
+
+function altera2(){
+    var imagem=document.getElementById("altera")
+    imagem.setAttribute('src', 'quebrada.jpg')
+}
